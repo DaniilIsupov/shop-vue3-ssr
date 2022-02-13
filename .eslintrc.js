fe6f0857db2e35c5cf7a50ec62a759366bd0c1e1
DaniilIsupov <disupov1996@gmail.com>
2022-02-13 21:42:09 +0200

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,15 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
-        // 'prettier',
-        // 'plugin:prettier/recommended',
+        'standard',
         'plugin:vue/vue3-recommended',
         'plugin:@typescript-eslint/recommended'
     ],
     rules: {
+        semi: ['warn', 'always'],
+        indent: ['warn', 4],
+        'space-before-function-paren': ['warn', 'never'],
+
         'vue/html-indent': ['warn', 4]
     }
 };
